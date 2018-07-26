526923336d6372a8dae1a47d15fa22c3c0ccf4da
Wezel Ball <wezel@nuc.ball.com>
2018-07-25 23:06:54 -0400

    Success with writing full 10msps airspy to hard disk, and reading into gnuradio from file.

--- airspy_gnuradio/README.txt
+++ airspy_gnuradio/README.txt
@@ -9,3 +9,19 @@ python plot_psd_base.py -d complex64 --sample-rate=2.5e6 --enable-spec
 I could use this for averaging plots, but recording data this way 
 consumes huge amounts of disk space.  I actually need to save the FFT
 data to disk, not the IQ data, but this is a start.
+
+
+I wote the grc app airspy_read_raw_iq.grc, where I recorded raw data at 10 msps to disk, and tried to read it
+under gnuradio, with following results:
+
+The airspy_read_raw_iq.grc is able to read the airspy file created with the following command:
+
+airspy_rx -t 0 -r ./airspyiq.dat -a 0 -f 88.9 -g 18 -n 50000000
+
+Format FLOAT32 I/Q (64 bits per sample)
+Sample rate 10e6 sps
+400MB disk space
+5.981 sec
+66 Mbytes disk space per second
+
+A 1TB hard drive would record 15,151 sec or 252 minutes or 4.2 hours of data.
--- airspy_gnuradio/top_block.py
+++ airspy_gnuradio/top_block.py
@@ -2,10 +2,8 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: airspy_gnuradio
-# Author: Dave Cohen
-# Description: Study in file output from Airspy
-# Generated: Wed Jul 25 22:09:17 2018
+# Title: Top Block
+# Generated: Wed Jul 25 22:44:18 2018
 ##################################################
 
 
@@ -21,87 +19,62 @@ if __name__ == '__main__':
 
 from gnuradio import blocks
 from gnuradio import eng_notation
-from gnuradio import filter
 from gnuradio import gr
 from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
 from gnuradio.fft import window
 from gnuradio.filter import firdes
-from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import waterfallsink2
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
-import osmosdr
-import time
 import wx
 
 
 class top_block(grc_wxgui.top_block_gui):
 
     def __init__(self):
-        grc_wxgui.top_block_gui.__init__(self, title="airspy_gnuradio")
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
         _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
         self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 2.5e6
+        self.samp_rate = samp_rate = 1e6
 
         ##################################################
         # Blocks
         ##################################################
-        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
+        self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_c(
         	self.GetWin(),
         	baseband_freq=0,
-        	y_per_div=10,
-        	y_divs=10,
+        	dynamic_range=100,
         	ref_level=0,
         	ref_scale=2.0,
-        	sample_rate=samp_rate/10,
-        	fft_size=1024,
+        	sample_rate=samp_rate,
+        	fft_size=8192,
         	fft_rate=15,
         	average=False,
         	avg_alpha=None,
-        	title='FFT Plot',
-        	peak_hold=False,
+        	title='Waterfall Plot',
         )
-        self.Add(self.wxgui_fftsink2_0.win)
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=1,
-                decimation=10,
-                taps=None,
-                fractional_bw=None,
-        )
-        self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + 'airspy=0' )
-        self.osmosdr_source_0.set_sample_rate(samp_rate)
-        self.osmosdr_source_0.set_center_freq(88.9e6, 0)
-        self.osmosdr_source_0.set_freq_corr(0, 0)
-        self.osmosdr_source_0.set_dc_offset_mode(2, 0)
-        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
-        self.osmosdr_source_0.set_gain_mode(True, 0)
-        self.osmosdr_source_0.set_gain(10, 0)
-        self.osmosdr_source_0.set_if_gain(20, 0)
-        self.osmosdr_source_0.set_bb_gain(20, 0)
-        self.osmosdr_source_0.set_antenna('', 0)
-        self.osmosdr_source_0.set_bandwidth(0, 0)
-
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/dcohen/Documents/devel/airspy_ra/airspy_gnuradio/airspy_iqdata.dat', False)
-        self.blocks_file_sink_0.set_unbuffered(False)
+        self.Add(self.wxgui_waterfallsink2_0.win)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/home/dcohen/airspyiq.dat', True)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.osmosdr_source_0, 0), (self.rational_resampler_xxx_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.rational_resampler_xxx_0, 0), (self.wxgui_fftsink2_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_waterfallsink2_0, 0))
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate/10)
-        self.osmosdr_source_0.set_sample_rate(self.samp_rate)
+        self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
 
 
 def main(top_block_cls=top_block, options=None):
